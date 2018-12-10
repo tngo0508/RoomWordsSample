2e35f129e756e7b201a035ac5f5f29284d79ac12
thomas <tngo0508@gmail.com>
2018-12-09 19:20:26 -0800

    Create the DAO

--- app/src/main/java/edu/fullerton/cpsc411/roomwordssample/Word.java
+++ app/src/main/java/edu/fullerton/cpsc411/roomwordssample/Word.java
@@ -8,7 +8,7 @@ import android.support.annotation.NonNull;
 @Entity(tableName = "word_table")
 public class Word {
 
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     @NonNull
     @ColumnInfo(name = "word")
     private String mWord;
